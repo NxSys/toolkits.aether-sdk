104d2a2096064c81e2ba9dc7fd8c21d605d7138e
wgraber <kal.zekdor@gmail.com>
2018-10-05 22:31:07 +0000

    [aether-sdk] * IException

--- src/Core/IException.php
+++ src/Core/IException.php
@@ -10,12 +10,12 @@
  * @link https://onx.zulipchat.com
  *
  * @package Aether
- * @subpackage System
+ * @subpackage SDK\Core
  * @license http://nxsys.org/spaces/aether/wiki/license
  * Please see the license.txt file or the url above for full copyright and license information.
  * @copyright Copyright 2018 Nexus Systems, inc.
  *
- * @author Chris R. Feamster <cfeamster@f2developments.com>
+ * @author William Graber <wgraber@nxsysts.com>
  * @author $LastChangedBy$
  *
  * @version $Revision$
@@ -27,21 +27,4 @@ namespace NxSys\Toolkits\Aether\SDK\Core;
 /** Local Project Dependencies **/
 use NxSys\Toolkits\Aether\SDK\Core;
 
-/** Framework Dependencies **/
-
-
-/** Library Dependencies **/
-
-
-/**
- * Undocumented class
- *
- * Why does this exist? What does this do?
- *
- * @throws NxSys\Toolkits\Aether\SDK\Core\IException Well, does it?
- * @author Chris R. Feamster <cfeamster@f2developments.com>
- */
- class ClassName extends AnotherClass implements Interface
- {
-
- }
+interface IException {}
