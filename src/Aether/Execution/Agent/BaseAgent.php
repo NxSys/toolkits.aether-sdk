3606b1ce2dcaa1e12862bdaa8fef3cd286b82a5c
wgraber <kal.zekdor@gmail.com>
2018-10-19 23:04:12 +0000

    [aether-sdk] * Release v0.2.0

---
+++ src/Aether/Execution/Agent/BaseAgent.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * $BaseName$
+ * $Id$
+ *
+ * DESCRIPTION
+ *  A Core file for Aether.sh
+ *
+ * @link http://nxsys.org/spaces/aether
+ * @link https://onx.zulipchat.com
+ *
+ * @package Aether
+ * @subpackage System
+ * @license http://nxsys.org/spaces/aether/wiki/license
+ * Please see the license.txt file or the url above for full copyright and license information.
+ * @copyright Copyright 2018 Nexus Systems, inc.
+ *
+ * @author Chris R. Feamster <cfeamster@f2developments.com>
+ * @author $LastChangedBy$
+ *
+ * @version $Revision$
+ */
+
+/** @namespace Native Namespace */
+namespace NxSys\Frameworks\Aether\Execution\Agents;
+
+/** Local Project Dependencies **/
+use NxSys\Frameworks\Aether;
+
+/** Framework Dependencies **/
+
+
+/** Library Dependencies **/
+use NxSys\Core\ExtensibleSystemClasses as CoreEsc;
+
+/**
+ * Undocumented class
+ *
+ * Why does this exist? What does this do?
+ *
+ * @throws NxSys\Frameworks\Aether\IException Well, does it?
+ * @author Chris R. Feamster <cfeamster@f2developments.com>
+ */
+ class BaseAgent extends CoreEsc\pthreads\Worker extends CoreEsc\pthreads\IAgent
+ {
+
+ }
---
+++ src/Aether/Execution/Supervisor.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * $BaseName$
+ * $Id$ rce.php 104 2018-02-23 23:44:25Z nxs.cfeamster $ rce.php 104 2018-02-23 23:44:25Z nxs.cfeamster $ rce.php 104 2018-02-23 23:44:25Z nxs.cfeamster $ rce.php 104 2018-02-23 23:44:25Z nxs.cfeamster $ rce.php 104 2018-02-23 23:44:25Z nxs.cfeamster $ rce.php 104 2018-02-23 23:44:25Z nxs.cfeamster $ rce.php 104 2018-02-23 23:44:25Z nxs.cfeamster $ rce.php 104 2018-02-23 23:44:25Z nxs.cfeamster $
+ *
+ * DESCRIPTION
+ *  Back Connector for RCEs * @copyright Copyright 2018 Nexus Systems, inc.
+ *
+ * @author Chris R. Feamster <cfeamster@f2developments.com>
+ * @author $LastChangedBy$
+ *
+ * @version $Revision$
+ */
+declare(strict_types=1);
+
+/** @namespace Native Namespace */
+namespace NxSys\Frameworks\Aether\Execution;
+
+/** Local Project Dependencies **/
+use NxSys\Frameworks\Aether;
+
+/** Framework Dependencies **/
+
+
+/** Library Dependencies **/
+use NxSys\Core\ExtensibleSystemClasses as CoreEsc;
+
+
+/**
+ * Undocumented class
+ *
+ * Why does this exist? What does this do?
+ *
+ * #singleton
+ *
+ * @throws NxSys\Frameworks\Aether\IException Well, does it?
+ * @author Chris R. Feamster <cfeamster@f2developments.com>
+ */
+ class Supervisor extends CoreEsc\pthreads\Pool
+ {
+
+ }
